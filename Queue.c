4f57b38d57ad0a48740047cdbd94ced3a1b3ac44
Nanboy-Ronan <jinruinan@163.com>
2021-06-06 16:44:39 -0700

    Update Queue.c (modify the function of resizing up and down).

--- Queue.c
+++ Queue.c
@@ -27,9 +27,28 @@ void enqueue(Queue* queue, ElementType ele){
     assert(queue);
     /* if full then resize my multiply a constant */
     if(queue->numOfElements == queue->capacity){
-        queue->a = realloc(queue->a, 2*queue->capacity*sizeof(ElementType));
+        ElementType* a = malloc(2*queue->capacity*sizeof(ElementType));
+        if(queue->front < queue->rear){
+            int index = 0;
+            for(int i = queue->front; i <= queue->rear; i++){
+                a[index] = queue->a[i];
+                index++;
+            }
+        }else{
+            int index = 0;
+            for(int i = queue->front; i <= queue->numOfElements - 1; i++){
+                a[index] = queue->a[i];
+                index++;
+            }
+            for(int i = 0; i <= queue->rear; i++){
+                a[index] = queue->a[i];
+                index++;
+            }
+        }
+        free(queue->a);
+        queue->a = a;
         queue->front = 0;
-        queue->rear = queue->numOfElements -1;
+        queue->rear = queue->numOfElements - 1;
         queue->capacity *= 2;
     }
     if(queue->front == -1 && queue->rear == -1){
@@ -44,22 +63,7 @@ void enqueue(Queue* queue, ElementType ele){
 
 ElementType dequeue(Queue* queue){
     assert(queue);
-    /* Resize if the size of the array is one quarter of its capacity */
-    if(queue->numOfElements < 0.25*queue->capacity){
-        int size = ceil(0.25*queue->capacity);
-        ElementType* a = malloc(size*sizeof(ElementType));
-        int index = 0;
-        for(int i = queue->front; i <= queue->rear; i++){
-            a[index]=queue->a[i];
-            index++;
-        }
-        free(queue->a);
-        queue->a = a;
-        queue->front = 0;
-        queue->rear = queue->numOfElements - 1;
-        queue->capacity = size;
-
-    }
+    
     ElementType temp;
     if(queue->numOfElements > 0){
         temp = queue->a[queue->front];
@@ -68,9 +72,39 @@ ElementType dequeue(Queue* queue){
             queue->rear = -1;
         }else{
             queue->front = (queue->front + 1) % queue->capacity;
-            queue->numOfElements--;
         }
+        queue->numOfElements--;
     }
+
+    /* Resize if the size of the array is one quarter of its capacity */
+    if(queue->numOfElements < 0.25*queue->capacity){
+        int size = ceil(0.25*queue->capacity);
+        ElementType* a = malloc(size*sizeof(ElementType));
+        if(queue->front < queue->rear){
+            int index = 0;
+            for(int i = queue->front; i <= queue->rear; i++){
+                a[index] = queue->a[i];
+                index++;
+            }
+        }else{
+            int index = 0;
+            for(int i = queue->front; i <= queue->numOfElements - 1; i++){
+                a[index] = queue->a[i];
+                index++;
+            }
+            for(int i = 0; i <= queue->rear; i++){
+                a[index] = queue->a[i];
+                index++;
+            }
+        }
+        free(queue->a);
+        queue->a = a;
+        queue->front = 0;
+        queue->rear = queue->numOfElements - 1;
+        queue->capacity = size;
+
+    }
+
     return temp;
 }
 
